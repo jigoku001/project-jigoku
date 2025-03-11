62e99f39e87729fee407db38d73f9422ce7b2957
yeico daza <kagemune.yd@hotmail.com>
2025-03-11 15:25:38 -0500

    prueba de subida

---
+++ personaje_prueba.gd
@@ -0,0 +1,28 @@
+class_name Prueba
+extends CharacterBody2D
+
+@export var gravity = 100
+@export var jump_speed = 100
+@export var speed = 100
+@onready var sprite:Sprite2D
+
+
+func _physics_process(delta):
+	
+	#horizontal
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = speed * direction
+	
+	if direction != 0:
+		$AnimatedSprite2D.scale.x = direction
+	
+	#gravedad
+	if not is_on_floor():
+		velocity.y = velocity.y + gravity * delta
+	
+	#saltar
+	var jump_pressed = Input.is_action_just_pressed("ui_up")
+	if jump_pressed:
+		velocity.y = velocity.y - jump_speed
+	
+	move_and_slide()
