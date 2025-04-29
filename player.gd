d113736b2678dc2ff374c0989f09040893193bb2
yeico daza <kagemune.yd@hotmail.com>
2025-04-29 16:55:49 -0500

    correccion carpetas / nuevos bocetos

---
+++ player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@export var move_speed : float
+@export var jump_speed : float
+@onready var animated_sprite = $AnimatedSprite2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_facing_right = true
+
+func _physics_process(delta):
+	
+	jump(delta)
+	move_x()
+	flip()
+	update_animation()
+	move_and_slide()
+
+func move_x():
+	var input_axix = Input.get_axis("ui_left","ui_right")
+	velocity.x = input_axix * move_speed
+	
+func flip():
+	if (is_facing_right and velocity.x < 0 ) or (not is_facing_right and velocity.x > 0):
+			scale.x *= -1
+			is_facing_right = not is_facing_right
+func jump(delta):
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_speed 
+	if not is_on_floor():	
+		velocity.y += gravity * delta
+
+func update_animation():
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("saltar")
+		else: 
+			animated_sprite.play("caer")
+		return
+		 
+	if velocity.x:
+		animated_sprite.play("caminar")
+	else:
+		animated_sprite.play("idle")
+	
+	
