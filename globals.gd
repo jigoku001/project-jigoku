c291dda73604452e4c6d7799d1bd2a374f320228
yeico daza <kagemune.yd@gmail.com>
2025-05-12 19:29:02 -0500

    interface de inicio

---
+++ globals.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var current_level : int = 1
+
+func _ready() -> void:
+	pass
+	
+func current_level_changed(level):
+	current_level = level
+func save_game():
+	var file = File.new()
+	file.open('res://data.save', file.WRITE)
