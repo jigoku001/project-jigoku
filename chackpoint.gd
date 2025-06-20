0af17379c8b2d2f6e2d2170673ab18187f2906c2
yeico daza <53058568+kagemune@users.noreply.github.com>
2025-06-20 00:40:04 -0500

    configuracion de muertes

---
+++ chackpoint.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var checkpoint_manager 
+
+func _ready() -> void:
+	checkpoint_manager= get_parent().get_node("game_controller")
+	
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		checkpoint_manager.last_location = $Marker2D.global_position
