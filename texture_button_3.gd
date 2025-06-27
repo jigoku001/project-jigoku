e1d5f87acc2290f8903078b2afae472b26dd0c23
yeico daza <kagemune.yd@gmail.com>
2025-06-27 15:28:24 -0500

    Trampas agregadas al nivel 3 (falta script de reinicio)

--- texture_button_3.gd
+++ texture_button_3.gd
@@ -3,9 +3,9 @@ extends TextureButton
 @export var level_proyect_dir1: String = "res://level_selector.tscn"
 
 func _on_pressed():
-	$"../../PixelArtResized1920x1080/AnimationPlayer".play("inicio")
+	$"../../AnimationPlayer".play("nivel")
 		
 func _on_animation_player_animation_finished(anim_name):
-	if (anim_name == "inicio"):
+	if (anim_name == "nivel"):
 		if level_proyect_dir1!= '':
 			get_tree().change_scene_to_file(level_proyect_dir1)
