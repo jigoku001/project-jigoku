a8ff52869468aea8da83829b421ebc13440f677b
yeico daza <53058568+kagemune@users.noreply.github.com>
2025-07-03 13:50:28 -0500

    manual

--- chackpoint.gd
+++ chackpoint.gd
@@ -1,20 +1,29 @@
 extends Area2D
 
 var checkpoint_manager 
-var easy_mode = Global.easy_mode
+var easy_mode = false
 
 func _ready() -> void:
 	
 	checkpoint_manager= get_parent().get_node("game_controller")
 	print(easy_mode)
 	if easy_mode == false:
-		$".".queue_free()
-	
-	
+		$".".monitoring=false
+		$".".monitorable=false
+		$".".visible = false
 
 	
+func _process(_delta: float) -> void:
+		
+		easy_mode = Global.easy_mode
+		
+		if easy_mode == true:
+			$".".monitoring=true
+			$".".monitorable=true
+			$".".visible = true
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		checkpoint_manager.last_location = $Marker2D.global_position
+		get_node("LogoInicio2").visible = false
